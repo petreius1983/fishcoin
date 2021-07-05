36949cf8685bf08d6b4218afae5b81408192c3cc
MacBook Air <macbookair@MacBook-Air-di-MacBook.local>
2021-07-05 23:45:45 +0200

    Updates

---
+++ precache-manifest.7963121cfcb0c9fdf23de30ca556a439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb0be176a36be4f5c141",
+    "url": "/fishcoin/static/css/main.fcb70ec4.chunk.css"
+  },
+  {
+    "revision": "fb0be176a36be4f5c141",
+    "url": "/fishcoin/static/js/main.fb0be176.chunk.js"
+  },
+  {
+    "revision": "faaa1679d43de39df8c5",
+    "url": "/fishcoin/static/css/1.b5cf4dfb.chunk.css"
+  },
+  {
+    "revision": "faaa1679d43de39df8c5",
+    "url": "/fishcoin/static/js/1.faaa1679.chunk.js"
+  },
+  {
+    "revision": "0b00286b6e48480ab59b",
+    "url": "/fishcoin/static/js/runtime~main.0b00286b.js"
+  },
+  {
+    "revision": "90a381bb1805a9ff85f4e836675f8449",
+    "url": "/fishcoin/index.html"
+  }
+];
